68a3e2ea203b855c2fdc91844e4f04cc24639bb5
EWS Gmbh & Co.KG <w.pallentin@pv.de>
2026-09-16 05:40:56 +0000

    Remove transition delay in GameShell

--- src/components/GameShell.jsx
+++ src/components/GameShell.jsx
@@ -53,17 +53,14 @@ function GameShellContent() {
             </div>
           )}
           <main className="flex-1 min-h-0 overflow-y-auto overflow-x-hidden">
-            <AnimatePresence mode="wait">
-              <motion.div
-                key={location.pathname}
-                initial={{ opacity: 0, y: 10 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -6 }}
-                transition={{ duration: 0.25, ease: EASE }}
-              >
-                <Outlet />
-              </motion.div>
-            </AnimatePresence>
+            <motion.div
+              key={location.pathname}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.2, ease: EASE }}
+            >
+              <Outlet />
+            </motion.div>
           </main>
           <ShellDock />
         </div>
